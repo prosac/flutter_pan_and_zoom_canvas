a3a64672745a898a9d275d88289646aa01b69c06
Johannes von Bargen <jo@mediaparadise.net>
2021-01-03 12:08:44 +0100

    fix path in import after renaming things

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:pan_and_zoom/work_bench.dart';
+import 'package:flutter_pan_and_zoom/work_bench.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
