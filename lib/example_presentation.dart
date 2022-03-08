a4d3d69008b015fb1a25d46a22082049c8e47745
Johannes von Bargen <jo@mediaparadise.net>
2022-03-08 21:33:30 +0100

    add offset, formaating, clean up

--- lib/example_presentation.dart
+++ lib/example_presentation.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 import 'presentation_container.dart';
 
@@ -7,28 +6,30 @@ class ExamplePresentation extends StatelessWidget {
   final label;
   final double width = 300;
   final double height = 200;
+  final Offset offset;
 
-  const ExamplePresentation({Key? key, this.label, this.color}) : super(key: key);
+  const ExamplePresentation({Key? key, this.label, this.color, required this.offset}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return PresentationContainer(
-      width: width,
-      height: height,
-      color: color,
-      label: label,
-      child: Center(
-        child:
+        width: width,
+        height: height,
+        color: color,
+        label: label,
+        child: Column(
+          children: [
+            Text(offset.toString()),
             NeumorphicButton(
               onPressed: () {
+                // ... skribbles
                 // var connections = context.read<ConnectionsModel>();
                 // print(connections.mode());
+                // var items = somehowGetTheItemsList();
+                // items.add(buildItem(offset, TestData(text: 'Lala')));
               },
               style: NeumorphicStyle(
-                shape: NeumorphicShape.flat,
-                boxShape: NeumorphicBoxShape.circle(),
-                color: Colors.grey.shade200
-              ),
+                  shape: NeumorphicShape.flat, boxShape: NeumorphicBoxShape.circle(), color: Colors.grey.shade200),
               padding: const EdgeInsets.all(20.0),
               child: Icon(
                 Icons.delete,
@@ -37,29 +38,11 @@ class ExamplePresentation extends StatelessWidget {
               duration: Duration(milliseconds: 50),
             ),
             // Padding(padding: EdgeInsets.only(top: 10)),
-      )
-    );
+          ],
+        ));
   }
 
   Color _iconsColor(BuildContext context) {
     return Colors.grey.shade400;
   }
-
-  // TODO: introduce themes!
-  // Color _iconsColor(BuildContext context) {
-  //   final theme = NeumorphicTheme.of(context);
-  //   if (theme.isUsingDark) {
-  //     return theme.current.accentColor;
-  //   } else {
-  //     return null;
-  //   }
-  // }
-  //
-  // Color _textColor(BuildContext context) {
-  //   if (NeumorphicTheme.isUsingDark(context)) {
-  //     return Colors.white;
-  //   } else {
-  //     return Colors.black;
-  //   }
-  // }
 }
