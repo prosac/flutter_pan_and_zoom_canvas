2699672607d542ba1972c4fd4174080f649bf823
Johannes von Bargen <jo@mediaparadise.net>
2022-07-10 18:07:02 +0200

    sort members

--- lib/work_bench.dart
+++ lib/work_bench.dart
@@ -12,11 +12,11 @@ import 'neumorphic_background.dart';
 import 'test_data.dart';
 
 class WorkBench extends StatefulWidget {
+  final double width;
+
+  final double height;
   WorkBench({Key? key, required this.width, required this.height})
       : super(key: key);
-
-  final double width;
-  final double height;
 
   @override
   WorkBenchState createState() => WorkBenchState();
@@ -33,13 +33,83 @@ class WorkBenchState extends State<WorkBench> {
   late Offset center;
   late MediaQueryData mediaQueryData;
 
-  @override
-  void initState() {
-    super.initState();
-    background =
-        NeumorphicBackground(width: widget.width, height: widget.height);
-    center = Offset(background.width / 2, background.height / 2);
-    resetViewport();
+  List get draggableItems {
+    GraphModel model = Provider.of<GraphModel>(context);
+
+    return model.nodes.map((Node node) {
+      Offset offset = node.offset;
+
+      return DraggableItem(
+          key: UniqueKey(),
+          offset: offset,
+          scale: scale,
+          node: node,
+          onDragStarted: () {
+            model.drag(node); // should implicitly do what setState does
+            model.startTicker(node); // should be implicit!
+          },
+          onDragCompleted: () {
+            model.stopTicker();
+          });
+    }).toList();
+  }
+
+  RenderBox get dragTargetRenderBox =>
+      dragTargetKey.currentContext!.findRenderObject() as RenderBox;
+
+  List<CustomPaint> get visualConnections {
+    GraphModel model = Provider.of<GraphModel>(context);
+
+    return model.connections.map((Edge connection) {
+      Size size1 = Size(connection.node1.presentation!.width,
+          connection.node1.presentation!.height);
+      Size size2 = Size(connection.node2.presentation!.width,
+          connection.node1.presentation!.height);
+
+      Offset nodeOffset1 = connection.node1.presentation!.offset;
+      Offset nodeOffset2 = connection.node2.presentation!.offset;
+
+      Offset offset1AdaptedToBackground = nodeOffset1;
+      Offset offset2AdaptedToBackground = nodeOffset2;
+
+      Offset offset1 = Offset(offset1AdaptedToBackground.dx + size1.width / 2,
+          offset1AdaptedToBackground.dy + size1.height / 2);
+      Offset offset2 = Offset(offset2AdaptedToBackground.dx + size2.width / 2,
+          offset2AdaptedToBackground.dy + size2.height / 2);
+
+      return CustomPaint(
+          painter: SimpleConnectionPainter(start: offset1, end: offset2));
+    }).toList();
+  }
+
+  // TODO: maybe this should be called something like GraphicalNodeRepresentation and thus graphicalNodeRepresentations
+  void addThing(model, offset) {
+    final newNode =
+        Node(offset: offset, payload: TestData(text: 'Some other Payload'));
+
+    // TODO: hot to best implement a bidirectional 1-1 relationsship
+    newNode.presentation = ExamplePresentation(
+        node: newNode,
+        onAddPressed: () => {addThingFromExisting(model, newNode)});
+
+    model.add(newNode);
+  }
+
+  void addThingFromExisting(GraphModel model, Node node) {
+    // final Offset offset = node.offset.translate(node.width, node.height);
+    final Offset offset = node.offset;
+    final adaptedOffset = computeAdaptedOffset(node, offset);
+
+    final newNode = Node(
+        offset: adaptedOffset, payload: TestData(text: 'Some other Payload'));
+
+    // TODO: hot to best implement a bidirectional 1-1 relationsship
+    newNode.presentation = ExamplePresentation(
+        node: newNode,
+        onAddPressed: () => {addThingFromExisting(model, newNode)});
+
+    model.add(newNode);
+    model.connections = Edge(node, newNode);
   }
 
   @override
@@ -99,62 +169,32 @@ class WorkBenchState extends State<WorkBench> {
     ]);
   }
 
+  Offset computeAdaptedOffset(Node node, Offset offset) {
+    var addition = Offset((node.width + 20) / background.width,
+        (node.height + 20) / background.height);
+
+    return offset + addition;
+  }
+
+  void deleteAllTheThings() {
+    Provider.of<GraphModel>(context, listen: false).removeAll();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    background =
+        NeumorphicBackground(width: widget.width, height: widget.height);
+    center = Offset(background.width / 2, background.height / 2);
+    resetViewport();
+  }
+
   void resetViewport() {
     var matrix = Matrix4.identity();
     matrix.translate(-center.dx, -center.dy);
     transformationController.value = matrix;
     setState(() => scale = 1.0);
     Provider.of<GraphModel>(context, listen: false).scale = 1.0;
-  }
-
-  // TODO: maybe this should be called something like GraphicalNodeRepresentation and thus graphicalNodeRepresentations
-  List get draggableItems {
-    GraphModel model = Provider.of<GraphModel>(context);
-
-    return model.nodes.map((Node node) {
-      Offset offset = node.offset;
-
-      return DraggableItem(
-          key: UniqueKey(),
-          offset: offset,
-          scale: scale,
-          node: node,
-          onDragStarted: () {
-            model.drag(node); // should implicitly do what setState does
-            model.startTicker(node); // should be implicit!
-          },
-          onDragCompleted: () {
-            model.stopTicker();
-          });
-    }).toList();
-  }
-
-  RenderBox get dragTargetRenderBox =>
-      dragTargetKey.currentContext!.findRenderObject() as RenderBox;
-
-  List<CustomPaint> get visualConnections {
-    GraphModel model = Provider.of<GraphModel>(context);
-
-    return model.connections.map((Edge connection) {
-      Size size1 = Size(connection.node1.presentation!.width,
-          connection.node1.presentation!.height);
-      Size size2 = Size(connection.node2.presentation!.width,
-          connection.node1.presentation!.height);
-
-      Offset nodeOffset1 = connection.node1.presentation!.offset;
-      Offset nodeOffset2 = connection.node2.presentation!.offset;
-
-      Offset offset1AdaptedToBackground = nodeOffset1;
-      Offset offset2AdaptedToBackground = nodeOffset2;
-
-      Offset offset1 = Offset(offset1AdaptedToBackground.dx + size1.width / 2,
-          offset1AdaptedToBackground.dy + size1.height / 2);
-      Offset offset2 = Offset(offset2AdaptedToBackground.dx + size2.width / 2,
-          offset2AdaptedToBackground.dy + size2.height / 2);
-
-      return CustomPaint(
-          painter: SimpleConnectionPainter(start: offset1, end: offset2));
-    }).toList();
   }
 
   void setScaleFromTransformationController() {
@@ -164,45 +204,5 @@ class WorkBenchState extends State<WorkBench> {
     model.scale = scale;
 
     model.offsetFromMatrix(transformationController.value);
-  }
-
-  void deleteAllTheThings() {
-    Provider.of<GraphModel>(context, listen: false).removeAll();
-  }
-
-  void addThing(model, offset) {
-    final newNode =
-        Node(offset: offset, payload: TestData(text: 'Some other Payload'));
-
-    // TODO: hot to best implement a bidirectional 1-1 relationsship
-    newNode.presentation = ExamplePresentation(
-        node: newNode,
-        onAddPressed: () => {addThingFromExisting(model, newNode)});
-
-    model.add(newNode);
-  }
-
-  void addThingFromExisting(GraphModel model, Node node) {
-    // final Offset offset = node.offset.translate(node.width, node.height);
-    final Offset offset = node.offset;
-    final adaptedOffset = computeAdaptedOffset(node, offset);
-
-    final newNode = Node(
-        offset: adaptedOffset, payload: TestData(text: 'Some other Payload'));
-
-    // TODO: hot to best implement a bidirectional 1-1 relationsship
-    newNode.presentation = ExamplePresentation(
-        node: newNode,
-        onAddPressed: () => {addThingFromExisting(model, newNode)});
-
-    model.add(newNode);
-    model.connections = Edge(node, newNode);
-  }
-
-  Offset computeAdaptedOffset(Node node, Offset offset) {
-    var addition = Offset((node.width + 20) / background.width,
-        (node.height + 20) / background.height);
-
-    return offset + addition;
   }
 }
