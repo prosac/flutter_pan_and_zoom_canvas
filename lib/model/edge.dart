7143ecc4d6d2efa7d71ca168c741bc4efe90f92c
Johannes von Bargen <jo@mediaparadise.net>
2022-07-12 00:16:24 +0200

    remove obsolete method

--- lib/model/edge.dart
+++ lib/model/edge.dart
@@ -6,11 +6,6 @@ class Edge {
   final Node node1;
   final Node node2;
 
-  @override
-  String toString() {
-    return '${node1.offset} ${node2.offset}';
-  }
-
   bool isConnectedTo(node) {
     return (node == node1 || node == node2);
   }
