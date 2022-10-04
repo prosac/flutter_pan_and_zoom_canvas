7729622b53f1142addd3dc31ed56fd3762ee1059
Johannes von Bargen <johannes.vonbargen@xing.com>
2022-10-04 23:33:37 +0200

    use neumorphic stuff as presentation container

--- lib/presentation_container.dart
+++ lib/presentation_container.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 
 class PresentationContainer extends StatelessWidget {
   final color;
@@ -7,42 +7,20 @@ class PresentationContainer extends StatelessWidget {
   final double? height;
   final Widget? child;
 
-  const PresentationContainer({Key? key, this.width, this.height, this.label, this.color, this.child}) : super(key: key);
+  const PresentationContainer(
+      {Key? key, this.width, this.height, this.label, this.color, this.child})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return FittedBox(
-      fit: BoxFit.contain,
-      child: Container(
-          width: width,
-          height: height,
-          padding: EdgeInsets.all(10.0),
-          // color: color, // TODO: how to work with color? maybe the injected color could act as the accent color?
-          child: child,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(16),
-              // color: Colors.grey.shade300, // TODO: how to work with color themes? i do not want to explicit color names all over the world
-              // https://api.flutter.dev/flutter/painting/LinearGradient-class.html
-              gradient: LinearGradient(
-                // begin: Alignment.topLeft,
-                // end: Alignment(0.8, 0.0), // 10% of the width, so there are ten blinds.
-                colors: [
-                  Colors.grey.shade300,
-                  Colors.grey.shade200
-                ],
-                // tileMode: TileMode.repeated, // repeats the gradient over the canvas
-              ),
-              boxShadow: [
-                BoxShadow(
-                    offset: Offset(10, 10),
-                    color: Colors.black38,
-                    blurRadius: 20),
-                BoxShadow(
-                    offset: Offset(-10, -10),
-                    color: Colors.white.withOpacity(0.85),
-                    blurRadius: 20)
-              ])
-      ),
-    );
+        fit: BoxFit.contain,
+        child: Neumorphic(
+          child: Container(
+              width: width,
+              height: height,
+              padding: EdgeInsets.all(10.0),
+              child: child),
+        ));
   }
 }
