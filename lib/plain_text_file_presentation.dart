2700a6bd449d5555da2916e582711a3a4a4f50c4
Johannes von Bargen <jo@mediaparadise.net>
2023-03-22 22:32:43 +0100

    enable plain text file presentation to load contents

--- lib/plain_text_file_presentation.dart
+++ lib/plain_text_file_presentation.dart
@@ -21,55 +21,56 @@ class PlainTextFilePresentation extends BasePresentation {
   Widget build(BuildContext context) {
     Widget presentation;
 
+    if (file.file.existsSync()) textEditingController.text = file.file.readAsStringSync();
+
     presentation = Consumer<ViewerState>(builder: (context, viewerState, child) {
       if (viewerState.scale > 0.5) {
         return PresentationContainer(
+            borderRadius: BorderRadius.all(Radius.zero),
             child: Material(
-          child: SizedBox(
-            width: width - 10,
-            height: height - 10,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Form(
-                  child: Builder(builder: (context) {
-                    return Column(
-                      children: [
-                        Text(file.path),
-                        TextFormField(
-                          controller: textEditingController,
-                          decoration: const InputDecoration(
-                            border: OutlineInputBorder(),
-                            filled: true,
-                            hintText: 'Write something...',
-                            labelText: 'Content',
-                          ),
-                          onTap: (() {
-                            viewerState.disableSpaceCommandMode();
-                          }),
-                          // onChanged: (value) => textEditingController.text = value,
-                          maxLines: 5,
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: ElevatedButton(
-                              onPressed: () async {
-                                await file.writeAsString(textEditingController.text);
+              child: SizedBox(
+                width: width,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Form(
+                      child: Builder(builder: (context) {
+                        return Column(
+                          children: [
+                            Text(file.path),
+                            TextFormField(
+                              controller: textEditingController,
+                              decoration: const InputDecoration(
+                                contentPadding: EdgeInsets.all(10),
+                                border: OutlineInputBorder(),
+                                filled: true,
+                                labelText: 'Notes',
+                              ),
+                              onTap: (() {
+                                viewerState.disableSpaceCommandMode();
+                              }),
+                              maxLines: 5,
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: ElevatedButton(
+                                  onPressed: () async {
+                                    await file.writeAsString(textEditingController.text);
 
-                                viewerState.requestFocus();
-                                viewerState.enableSpaceCommandMode();
-                              },
-                              child: Text('Save')),
-                        )
-                      ],
-                    );
-                  }),
+                                    viewerState.requestFocus();
+                                    viewerState.enableSpaceCommandMode();
+                                  },
+                                  child: Text('Save')),
+                            )
+                          ],
+                        );
+                      }),
+                    ),
+                  ],
                 ),
-              ],
-            ),
-          ),
-        ));
+              ),
+            ));
       }
 
       return Container(
