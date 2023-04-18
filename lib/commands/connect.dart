e2abf4a71106998ce803e6321563d80594360eae
Johannes von Bargen <johannes.vonbargen@xing.com>
2023-04-19 01:00:08 +0200

    also delete sources

--- lib/commands/connect.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_pan_and_zoom/model/graph_model.dart';
-import 'package:flutter_pan_and_zoom/model/node.dart';
-import 'package:provider/provider.dart';
-
-void connect({required Node otherNode, required BuildContext context}) {
-  final model = context.read<GraphModel>();
-  model.addEdgeTo(otherNode);
-}
--- lib/commands/initiate_connecting.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_pan_and_zoom/model/graph_model.dart';
-import 'package:flutter_pan_and_zoom/model/node.dart';
-import 'package:provider/provider.dart';
-
-void initiateConnecting({required Node fromNode, required BuildContext context}) {
-  final model = context.read<GraphModel>();
-  model.nodeToConnect = fromNode;
-}
